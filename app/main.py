bba7098a38f5d87c8428964737156d46b7d45277
isikepalaku <isikepalakreatif@gmail.com>
2025-03-17 04:32:49 +0800

    fix corss

--- app/main.py
+++ app/main.py
@@ -24,7 +24,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3001",
         "http://127.0.0.1:3001",
-        "https://cdr.reverse.id",
+        "https://cdr.reserse.id",
         "http://localhost:8080"
     ],
     allow_credentials=True,
